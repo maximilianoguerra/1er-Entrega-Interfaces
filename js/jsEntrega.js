e0c74c77c85391a969babdf19cf2940c0ef280ca
Maximiliano Guerra <guerramdg@gmail.com>
2018-08-25 15:31:00 -0300

    agregue carpetas pinto mas pixeles

--- js/jsEntrega.js
+++ js/jsEntrega.js
@@ -4,33 +4,63 @@ $(document).on('click','#lapiz',function (event) {
   cambiarALapiz();
 });
 function cambiarALapiz() {
-  document.body.style.cursor =  "url(lapiz32p.png),auto";
+  document.body.style.cursor =  "url(img/lapiz32p.png),auto";
+  canvas.addEventListener('mousemove', function(event) {
+      let imageData = ctx.getImageData(0,0,1000,1000);
+      setPixel(imageData,event.layerX,event.layerY,0,0,0,255);
+      setPixel(imageData,event.layerX,event.layerY-1,0,0,0,255);
+      setPixel(imageData,event.layerX,event.layerY+1,0,0,0,255);
+      setPixel(imageData,event.layerX-1,event.layerY,0,0,0,255);
+      setPixel(imageData,event.layerX-1,event.layerY-1,0,0,0,255);
+      setPixel(imageData,event.layerX-1,event.layerY+1,0,0,0,255);
+      setPixel(imageData,event.layerX+1,event.layerY,0,0,0,255);
+      setPixel(imageData,event.layerX+1,event.layerY-1,0,0,0,255);
+      setPixel(imageData,event.layerX+1,event.layerY+1,0,0,0,255);
+      ctx.putImageData(imageData,0,0);
+
+    }, false);
 }
 $(document).on('click','#goma',function (event) {
   event.preventDefault();
   cambiarAGoma();
 });
 function cambiarAGoma() {
-  document.body.style.cursor =  "url(goma-de-borrar32p.png),auto";
+  document.body.style.cursor =  "url(img/goma-de-borrar32p.png),auto";
+  canvas.addEventListener('mousemove', function(event) {
+      let imageData = ctx.getImageData(0,0,1000,1000);
+      setPixel(imageData,event.layerX,event.layerY,255,255,255,255);
+      setPixel(imageData,event.layerX,event.layerY-1,255,255,255,255);
+      setPixel(imageData,event.layerX,event.layerY+1,255,255,255,255);
+      setPixel(imageData,event.layerX-1,event.layerY,255,255,255,255);
+      setPixel(imageData,event.layerX-1,event.layerY-1,255,255,255,255);
+      setPixel(imageData,event.layerX-1,event.layerY+1,255,255,255,255);
+      setPixel(imageData,event.layerX+1,event.layerY,255,255,255,255);
+      setPixel(imageData,event.layerX+1,event.layerY-1,255,255,255,255);
+      setPixel(imageData,event.layerX+1,event.layerY+1,255,255,255,255);
+      ctx.putImageData(imageData,0,0);
+
+    }, false);
 }
 var ctx = document.getElementById("canvas").getContext("2d");
-function myDrawImageMethod(image) {
-  ctx.drawImage(image,0,0,700,700);
+let imageData = ctx.createImageData(1000,1000);
+for (var x = 0; x < imageData.width; x++) {
+  for (var y = 0; y < imageData.width; y++) {
+    setPixel(imageData,x,y,255,255,255,255);
+  }
 };
-let image1 = new Image();
-image1.src="enBlanco.png"
-image1.onload = function(){
-  myDrawImageMethod(this);
-  let imageData = ctx.getImageData(0,0,700,700);
-  // tonos(imageData);
-  ctx.putImageData(imageData,0,0);
-};
-canvas.addEventListener('mousemove', function(event) {
-    let imageData = ctx.getImageData(0,0,1000,1000);
-    setPixel(imageData,event.layerX,event.layerY,0,0,0,255);
-    ctx.putImageData(imageData,0,0,0,0,1000,1000);
+ctx.putImageData(imageData,10,10);
+// function myDrawImageMethod(image) {
+//   ctx.drawImage(image,0,0,700,700);
+// };
+// let image1 = new Image();
+// image1.src="enBlanco.png"
+// image1.onload = function(){
+//   myDrawImageMethod(this);
+//   let imageData = ctx.getImageData(0,0,700,700);
+//   // tonos(imageData);
+//   ctx.putImageData(imageData,0,0);
+// };
 
-  }, false);
   function setPixel(imageData,x,y,r,g,b,a) {
     index = (x + y * imageData.width) * 4;
     imageData.data[index+0]=r;
